7b3923995aa74aec6c2d5c90978f2b9957bee1f2
Carlo Kruger <ironicbuddha@gmail.com>
2023-12-28 14:54:04 +0200

    replacing some text about posts

--- app/(website)/archive/page.js
+++ app/(website)/archive/page.js
@@ -12,11 +12,11 @@ export default async function ArchivePage({ searchParams }) {
     <>
       <Container className="relative">
         <h1 className="text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
-          Archive
+          music
         </h1>
         <div className="text-center">
           <p className="mt-2 text-lg">
-            See all posts we have ever written.
+            here is all my music
           </p>
         </div>
         <Suspense
