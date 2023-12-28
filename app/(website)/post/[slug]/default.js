0f1f3263203ca526cbd439b339b70bb0ef61fbf8
Carlo Kruger <ironicbuddha@gmail.com>
2023-12-28 14:34:44 +0200

    changed the link text

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -98,7 +98,7 @@ export default function Post(props) {
             <Link
               href="/"
               className="bg-brand-secondary/20 rounded-full px-5 py-2 text-sm text-blue-600 dark:text-blue-500 ">
-              ← View all posts
+              ← View all music
             </Link>
           </div>
           {post.author && <AuthorCard author={post.author} />}
